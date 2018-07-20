d4a8fb911b63e92d3ee5cfcf12daa73a7768dd4a
Martin Alonso <m13alonso@gmail.com>
2018-07-21 00:44:38 +0200

    Feature: add gender to characters

--- bas/db/model/character.py
+++ bas/db/model/character.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Column, Integer, Sequence, String
+from sqlalchemy import Column, Integer, Sequence, String, ForeignKey, \
+        BigInteger
+from sqlalchemy.orm import relationship
 
 from bas.db.database import Base
 
@@ -7,9 +9,19 @@ class Character(Base):
     __tablename__ = 'characters'
     id = Column(Integer, Sequence('character_id_seq'), primary_key=True)
     name = Column(String(500))
+    gender_id = Column(Integer, ForeignKey('genders.id'))
+    gender = relationship('Gender')
+    experience = Column(BigInteger)
+    level = Column(Integer)
 
-    def __init__(self, name):
+    def __init__(self, name, gender=None, level=1):
         self.name = name
+        self.gender = gender
+        self.level = level
 
     def __repr__(self):
-        return '<Character(id={}, name={})>'.format(self.id, self.name)
+        return '<Character(id={}, name={}, gender={})>'.format(
+            self.id,
+            self.name,
+            self.gender,
+        )
