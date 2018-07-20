fa0c41beff42a77af867741af978cc0c39467d38
Martin Alonso <m13alonso@gmail.com>
2018-07-21 00:44:20 +0200

    Fix: add metadata naming convention to support foreign key migrations

--- bas/db/database.py
+++ bas/db/database.py
@@ -1,10 +1,18 @@
-from sqlalchemy import create_engine
+from sqlalchemy import MetaData, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.pool import StaticPool
 
 
-Base = declarative_base()
+meta = MetaData(naming_convention={
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+})
+
+Base = declarative_base(metadata=meta)
 
 
 class Database:
