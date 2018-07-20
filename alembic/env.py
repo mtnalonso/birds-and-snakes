3d603f1fa53feaf860ed1c9cb7dfe453076034dc
Martin Alonso <m13alonso@gmail.com>
2018-07-21 00:10:24 +0200

    Fix: enable alembic to perform alter actions with sqlite

--- alembic/env.py
+++ alembic/env.py
@@ -62,7 +62,8 @@ def run_migrations_online():
     with connectable.connect() as connection:
         context.configure(
             connection=connection,
-            target_metadata=target_metadata
+            target_metadata=target_metadata,
+            render_as_batch=config.get_main_option('sqlalchemy.url').startswith('sqlite:///')
         )
 
         with context.begin_transaction():
