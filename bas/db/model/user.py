47053976b610579f5b9aab56398827145d1db96b
Martin Alonso <m13alonso@gmail.com>
2018-07-27 02:33:12 +0200

    Feature: add user level and experience

--- bas/db/model/user.py
+++ bas/db/model/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Table, Column, Integer, String, Sequence
+from sqlalchemy import Table, Column, Integer, String, Sequence, BigInteger
 from sqlalchemy.orm import relationship
 
 from bas.db.database import Base
@@ -9,6 +9,9 @@ class User(Base):
     id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
     username = Column(String(100))
     twitter_username = Column(String(50))
+    level = Column(Integer)
+    max_character_level = Column(Integer)
+    total_experience = Column(BigInteger)
 
     games = relationship('Game', secondary='game_users')
     characters = relationship('Character')
@@ -18,8 +21,10 @@ class User(Base):
         self.twitter_username = twitter_username
 
     def __repr__(self):
-        return '<User(id={}, username={}, twitter_username={})>'.format(
-            self.id, self.username, self.twitter_username
+        return '''
+                <User(id={}, username={}, twitter_username={}, level={})>
+                '''.format(
+            self.id, self.username, self.twitter_username, self.level
         )
 
     def get_active_games(self):
