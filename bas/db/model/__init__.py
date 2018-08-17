79b4995c541e2280b574113b3a4aefd25f68685e
Martin Alonso <m13alonso@gmail.com>
2018-08-17 22:13:09 +0200

    Feature: add level entity to model

--- bas/db/model/__init__.py
+++ bas/db/model/__init__.py
@@ -8,6 +8,7 @@ from bas.db.model.game import Game
 from bas.db.model.game_state import GameState
 from bas.db.model.gender import Gender
 from bas.db.model.game_users import GameUsers
+from bas.db.model.level import Level
 from bas.db.model.magic_school import MagicSchool
 from bas.db.model.pronoun import Pronoun
 from bas.db.model.race import Race
---
+++ bas/db/model/level.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, Sequence, String
+
+from bas.db.database import Base
+
+
+class Level(Base):
+    __tablename__ = 'levels'
+    id = Column(Integer, Sequence('level_id_seq'), primary_key=True)
+    name = Column(String(200))
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return '<Level(id={}, name={})>'.format(self.id, self.name)
