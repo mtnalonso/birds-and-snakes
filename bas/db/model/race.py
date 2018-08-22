d9552217d90a7f965268d0f17cc5b830f8763095
Martin Alonso <m13alonso@gmail.com>
2018-08-23 00:00:03 +0200

    Feature: add speed to race

--- bas/db/model/race.py
+++ bas/db/model/race.py
@@ -7,9 +7,11 @@ class Race(Base):
     __tablename__ = 'races'
     id = Column(Integer, Sequence('race_id_seq'), primary_key=True)
     name = Column(String(100))
+    speed = Column(Integer)
 
-    def __init__(self, name):
+    def __init__(self, name, speed=None):
         self.name = name
+        self.speed = speed
 
     def __repr__(self):
         return '<Race(id={}, name={})>'.format(self.id, self.name)
